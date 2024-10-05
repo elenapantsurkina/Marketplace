060b83b994f35f2bef76e799e723f763e8e74cf8
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-10-05 16:20:03 +0700

    Созданы шаблоны Реализованы контроллеры

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
+from catalog.views import home
+from catalog.views import contacts
+
 
 app_name = CatalogConfig.name
 
-urlpattern = [
-    path('',)
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
 ]
