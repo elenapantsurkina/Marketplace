a6da2dc6606e7d9bcc225425afaac86a52561992
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-10-09 18:15:00 +0700

    создана БД marketplace созданы модели Product и Category

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование продукта",
+        help_text="Введите наименование продукта",
+    )
+    description = models.TextField(
+        verbose_name="Описание продукта",
+        help_text="Введите описание продукта",
+        blank=True,
+        null=True,
+    )
+    image = models.ImageField(
+        upload_to="product/image",
+        blank=True,
+        null=True,
+        verbose_name="Фото продукта",
+        help_text="Загрузите фото продукта",
+    )
+    category = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(
+        auto_now=True, verbose_name="Дата последнего изменения"
+    )
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
+        ordering = ["name", "description", "category"]
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование категории",
+        help_text="Введите наименование категории",
+    )
+    description = models.TextField(
+        verbose_name="Описание категории",
+        help_text="Введите описание категории",
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
--- config/settings.py
+++ config/settings.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
 from pathlib import Path
+
+load_dotenv(override=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-)ec5*gslm0o#wohvb--f^1p9&nl*-*-daxmi99w*lm8bz^rybx'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == 'True' else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -51,8 +55,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('NAME'),
+        'USER': os.getenv('USER'),
+        'PASSWORD': os.getenv('PASSWORD'),
+        'HOST': os.getenv('HOST'),
+        'PORT': os.getenv('PORT'),
     }
 }
 
