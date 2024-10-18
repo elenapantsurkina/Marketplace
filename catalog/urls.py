cb7c1c936d8837c286953f67086ae44c8b82fd3e
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-10-18 20:52:16 +0700

    реализовано отображение товаров на станице главная

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,10 +4,12 @@ from catalog.views import home
 from catalog.views import contacts
 
 
+
 app_name = CatalogConfig.name
 
 
 urlpatterns = [
     path('', home, name='home'),
     path('contacts/', contacts, name='contacts'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
 
 def home(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, 'products.html', context)
 
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+
+# def index(request):
+#     return render(request, 'base.html')
