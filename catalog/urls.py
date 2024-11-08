b2884816002d0d6c0f151fbdc2d1e09f965fb973
elena_pantsurkina <pantsurkina.e@bk.ru>
2024-11-08 20:59:29 +0700

    скорректирован механизм CRUD для модели продуктов

--- catalog/urls.py
+++ catalog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path(
         "product_delete/<int:pk>/",
         ProductDeleteView.as_view(),
-        name="product_confirm_delete",
+        name="product_delete",
     ),
     path(
         "product_update/<int:pk>/", ProductUpdateView.as_view(), name="product_update"
